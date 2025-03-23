81eac433fb90cf23209e08d947fe6ae6c08a9507
IIJaredII <jaredrios2425@gmail.com>
2025-03-22 18:55:41 -0600

    Se añadiron las funcions de crear y optener contactos en el controlador y se configuraron las rutas

--- src/controllers/contactos.controller.js
+++ src/controllers/contactos.controller.js
@@ -1,2 +1,51 @@
 const fs = require("fs");
 const connection = require("../config/db");
+
+const crearContacto = async (req,res)  => {
+    try {
+        
+        const {nombre,idPais,latitud,longitud} = req.body;
+        const video = req.file ? req.file.path.replace(/\\/g, "/") : null;
+
+        if(!nombre || !idPais || !latitud || !longitud || !video) {
+            return res.status(0).send()
+        }
+
+        const results = await connection.promise.query(
+            "CALL insertContacto(?,?,?,?,?)",
+            [nombre,idPais,latitud,longitud,video]
+        );
+
+        res.status(201).json({
+            mensaje: "Contacto agregado correctamente",
+            id: results.insertId
+        });
+
+    } catch (error) {
+        console.error("Error al crear contacto:", error);
+        
+                if (req.file) {
+                    try {
+                        fs.unlinkSync(req.file.path);
+                        console.log("Video eliminada debido a un error en la BD.");
+                    } catch (unlinkError) {
+                        console.error("Error al eliminar el video:", unlinkError);
+                    }
+                }
+        
+                res.status(500).json({ mensaje: "Error al crear el contacto" });
+    }
+};
+
+const obtenerContacto = async (req, res) => {
+    try {
+        const [results] = await connection.promise().query('CALL obtenerContactos()');
+
+        res.json(results[0]); 
+    }catch (error) {
+        console.error("Error al obtener contactos:", error);
+        res.status(500).json({ mensaje: "Error al obtener contactos" });
+    }
+}
+
+module.exports = {crearContacto,obtenerContacto};
--- src/routes/contactos.routes.js
+++ src/routes/contactos.routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const contactos = require("../controllers/contactos.controller");
 const upload = require("../middlewares/multerConfig");
-const {verifyToken,checkRole} = require("../middlewares/authConfig");
+const {verifyToken} = require("../middlewares/authConfig");
 
-router.get("", );
-router.post("", );
+router.get("/",verifyToken,contactos.obtenerContacto);
+router.post("/",verifyToken,upload.single(""),contactos.crearContacto);
 router.delete("", );
 router.put("", );
 
