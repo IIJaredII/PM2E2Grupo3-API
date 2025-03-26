d7283db72b61db56cbfe988b6fdbc4b014fdc476
IIJaredII <jaredrios2425@gmail.com>
2025-03-26 02:17:48 -0600

    asdasdasafsdfsdf

--- src/controllers/contactos.controller.js
+++ src/controllers/contactos.controller.js
@@ -71,7 +71,7 @@ const obtenerContactoPorNombre = async (req, res) => {
     try {
         const {nombre} = req.params;
         const [results] = await connection.promise().query(
-            "SELECT id, nombre, codigo, telefono, longitud, latitud FROM contactos WHERE nombre LIKE CONCAT('%', ?, '%')"
+            "SELECT id, nombre, idPais, telefono, longitud, latitud FROM contactos WHERE nombre LIKE CONCAT('%', ?, '%')"
             ,[nombre]);
 
         if (results.length === 0) {
